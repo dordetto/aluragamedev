858dc101a1732fe02f50200c10f1eb061c72cf31
dordetto <32464859+dordetto@users.noreply.github.com>
2020-08-04 23:17:06 -0300

    Readme improvements

--- scripts/jogo/cenas/telainicial.js
+++ scripts/jogo/cenas/telainicial.js
@@ -1,42 +1,35 @@
 class TelaInicial {
-  constructor() {}
+    constructor(){
+    }
 
-  draw() {
-    this._imagemDeFundo();
-    this._texto();
-    this._botao();
-  }
+    draw(){
+        this._imagemDeFundo();
+        this._texto();
+        this._botao();
+    }
 
-  _imagemDeFundo() {
-    image(imagemTelaInicial, 0, 0, width, height);
-  }
+    _imagemDeFundo(){
+        image(imagemTelaInicial, 0, 0, width, height)
+    }
 
-  _texto() {
-    textFont(fonteTelaInicial);
-    textAlign(CENTER);
-    textSize(70);
-    text("As aventuras da Bruxinha", width / 2, height / 4);
-    text("Marininha", width / 2, height / 4 + 50);
+    _texto(){
+        textFont(fonteTelaInicial)
+        textAlign(CENTER)
+        textSize(70);
+        text('As aventuras da Bruxinha', width /2, height/4 )
+        text('Marininha', width /2, height/4 + 50)
 
-    textFont("Arial");
-    textAlign(LEFT);
-    textSize(20);
-    text(
-      "Ajude a Bruxinha atravesar a floresta sem se contaminar!",
-      20,
-      (height / 5) * 4
-    );
-    text(
-      "As gotas de choro do Troll estão contaminadas, desvie!!!!",
-      20,
-      (height / 5) * 4 + 20
-    );
-    text("A Vida diminui em contato com eles.", 20, (height / 5) * 4 + 40);
-    text("Ganhe Vida coletando Morcegos.", 20, (height / 5) * 4 + 60);
-    text(`Pulo: Enter ou Up (aceita pulo duplo)`, 20, (height / 5) * 4 + 80);
-  }
+        textFont('Arial')
+        textAlign(LEFT)
+        textSize(20);
+        text('Ajude a Bruxinha atravesar a floresta sem se contaminar!', 20, height/5 * 4)
+        text('As gostas de choro do Troll são contaminadas, desvie!!!!', 20, height/5 * 4 + 20)
+        text('A Vida diminui em contato com eles.', 20, height/5 * 4 + 40)
+        text('Ganhe Vida coletando Morcegos', 20, height/5 * 4 + 60)
+        text(`Pulo: Enter ou Up (aceita pulo duplo)`, 20, height/5 * 4 + 80)
+    }
 
-  _botao() {
-    botao.draw();
-  }
+    _botao(){
+        botao.draw()
+    }
 }
